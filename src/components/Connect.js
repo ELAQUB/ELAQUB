a31e421a3b30dcbb92c71319fec5612869539348
Chibuzo Franklin Odigbo <cfaodigbo@gmail.com>
2022-12-31 10:01:01 +0100

    added images to connect

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import '../sass/Connect.scss'
+import one from '../assets/images/edit1.png'
+import two from '../assets/images/edit2.png'
+import three from '../assets/images/edit3.png'
+import four from '../assets/images/edit4.png'
+import five from '../assets/images/edit5.png'
+import six from '../assets/images/edit6.png'
+import seven from '../assets/images/edit7.png'
 
 const Connect = () => {
   return (
@@ -15,13 +22,27 @@ const Connect = () => {
             <div className="right"></div>
         </div>
         <div className="circles">
-            <div className="one"></div>
-            <div className="two"></div>
-            <div className="three"></div>
-            <div className="four"></div>
-            <div className="five"></div>
-            <div className="six"></div>
-            <div className="seven"></div>
+            <div className="one">
+              <img src={one} alt="" />
+            </div>
+            <div className="two">
+            <img src={two} alt="" />
+            </div>
+            <div className="three">
+            <img src={three} alt="" />
+            </div>
+            <div className="four">
+            <img src={four} alt="" />
+            </div>
+            <div className="five">
+            <img src={five} alt="" />
+            </div>
+            <div className="six">
+            <img src={six} alt="" />
+            </div>
+            <div className="seven">
+            <img src={seven} alt="" />
+            </div>
         </div>
     </div>
   )
